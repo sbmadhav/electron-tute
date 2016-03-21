1f88688deb3f943a643c8d9f9c3751ba8eae17e3
Brameshmadhav Srinivasan <sbmadhav@gmail.com>
2016-03-21 05:23:28 -0700

    Step 1 : Hello world

---
+++ index.js
@@ -0,0 +1,29 @@
+const electron = require( 'electron' )
+    , app = electron.app
+    , BrowserWindow = electron.BrowserWindow
+    , screenWidth = 800
+    , screenHeight = 600
+    ;
+var mainWindow = null;
+  ;
+app.on( 'ready', function () {
+    mainWindow = new BrowserWindow ({
+          width: screenWidth
+        , height: screenHeight
+        , show: false
+        // some additional params
+        , 'always-on-top': true
+    });
+
+    // for GC
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+    });
+
+    mainWindow.loadUrl( 'file://' + __dirname + '/index.html' );
+    mainWindow.show();
+})
+.on('window-all-closed', function() {
+  app.quit();
+})
+;
